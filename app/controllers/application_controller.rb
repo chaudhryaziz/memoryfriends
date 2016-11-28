8009fcaae9324fc5bf4cc3715e281e4b138d0dc2
Aziz Chaudhry <chaudhryaziz@gmail.com>
2016-11-28 11:12:11 -0500

    Moved to Rails 5.0.0.1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
-  def intro
-    render html: "Welcome to Memory Friends, this is an app to help you map friends from memory."
-  end
 end
