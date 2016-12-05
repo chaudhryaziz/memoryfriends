a0dd4b38b66db76b735d2c62272e2ba151c3cb1f
Aziz Chaudhry <chaudhryaziz@gmail.com>
2016-12-04 23:47:13 -0500

    Added friendship model from fork number 3a

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pages#home'
 
-  resources :users, only: [:show]
+  resources :users, only: [:show, :index]
+  resources :friendships, only: [:create, :destroy, :accept] do
+  	member do 
+  		put :accept
+  	end
+  	end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+User.create(username: "ricouzuki", email: "chaudhryaziz@gmail.com", password: "hamilton26AA", password_confirmation: "hamilton26AA")
+User.create(username: "ricouzuki88", email: "chaudhryaziz88@gmail.com", password: "hamilton26AA", password_confirmation: "hamilton26AA")
+User.create(username: "ricouzuki89", email: "chaudhryaziz89@gmail.com", password: "hamilton26AA", password_confirmation: "hamilton26AA")
+User.create(username: "ricouzuki90", email: "chaudhryaziz90@gmail.com", password: "hamilton26AA", password_confirmation: "hamilton26AA")
+User.create(username: "ricouzuki91", email: "chaudhryaziz91@gmail.com", password: "hamilton26AA", password_confirmation: "hamilton26AA")
+
+p "Test users created"
