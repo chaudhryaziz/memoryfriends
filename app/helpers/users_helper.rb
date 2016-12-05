7df65b907071263c4f6561b639654dd764875436
Aziz Chaudhry <chaudhryaziz@gmail.com>
2016-12-05 00:38:36 -0500

    Added user detection, cleaned up some css

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,7 +1,7 @@
 module UsersHelper
 	def action_buttons(user)
 		case current_user.friendship_status(user) when "friends"
-			link_to "Cancel Friendship", friendship_path(current_user.friendship_relation(user)), method: :delete
+			link_to "Unfriend", friendship_path(current_user.friendship_relation(user)), method: :delete
 		when "pending"
 			link_to "Cancel Request", friendship_path(current_user.friendship_relation(user)), method: :delete
 		when "requested"
