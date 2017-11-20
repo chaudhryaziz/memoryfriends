00100ca55d2e7a055f7e3bb780357c72406ec7b1
Aziz Chaudhry <chaudhryaziz@gmail.com>
2017-11-20 16:32:16 -0500

    Revised look and feel

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {registrations: 'registrations'}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pages#home'
+  get 'about' => "pages#about"
 
   resources :users, only: [:show, :index]
   resources :friendships, only: [:create, :destroy, :accept] do
